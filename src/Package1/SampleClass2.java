d4b6923a2ddb19a316b8d041f5cb75cb893eed78
Guruanna <44716483+ANREMS@users.noreply.github.com>
2025-09-16 10:39:53 +0100

    Create SampleClass2.java
    SampleClass2 added from GIT by SS

---
+++ src/Package1/SampleClass2.java
@@ -0,0 +1,7 @@
+public class SampleClass2 {
+
+    public static void main(String[] args) {
+        System.out.println("SampleClass2 created from GIT");
+         System.out.println("Code added from GIT");
+    }
+}
