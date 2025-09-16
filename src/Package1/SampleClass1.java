4070420ab244748273c2f01c570b5aa7cfdb8109
Guruanna <44716483+ANREMS@users.noreply.github.com>
2025-09-16 10:38:18 +0100

    Update SampleClass1.java
    Additional code added by SS

--- src/Package1/SampleClass1.java
+++ src/Package1/SampleClass1.java
@@ -4,5 +4,6 @@ public class SampleClass1 {
 
     public static void main(String[] args) {
         System.out.println("Hello from SampleClass1 in Package1");
+         System.out.println("Additional code added");
     }
 }
