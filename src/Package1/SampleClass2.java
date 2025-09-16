a2cf177c672770b09e74ffe833b5ea5c58d999fe
Anna <choannadurai@gmail.com>
2025-09-16 10:45:02 +0100

    Updated code Commit by SS

--- src/Package1/SampleClass2.java
+++ src/Package1/SampleClass2.java
@@ -1,7 +1,10 @@
+package Package1;
+
 public class SampleClass2 {
 
     public static void main(String[] args) {
         System.out.println("SampleClass2 created from GIT");
          System.out.println("Code added from GIT");
+        System.out.println("Code updated from local");
     }
 }
